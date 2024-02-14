1c92d6c976d77ae6e583aee39ee2114552410050
Philippe THOMY <philippe@loco-labs.io>
2024-02-15 00:53:41 +0100

    test numpy

--- python/Tests/test_numpy.py
+++ python/Tests/test_numpy.py
@@ -6,68 +6,65 @@ Created on Tue Feb 13 15:54:47 2024
 """
 
 import numpy as np
-from cfield import Cutil
-from dataset import Sdataset
-from field import Sfield
-import ntv_pandas as npd
+
+from field import Sfield, Nfield
+
 from ntv_util import NtvUtil, NtvConnector
 from json_ntv import Ntv
 
-def to_json(ndarray, axes=None, name=None, ntv=None ):
-    
-    ntv_t = ':xndarray' if axes and ntv == 'ndarray' else ':ndarray' if ntv== 'ndarray' else ':tab' if ntv == 'tab' else ''
-    if ntv_t == ':tab':
-        value = to_json_tab(ndarray, axes, name)
-        name = ntv_t
-    else: 
-        #name = ntv_t if not name else name + ntv_t
-        name = '' if not name else name
-        data = {name + ':' + ndarray.dtype.name: ndarray.tolist()}
-        value = data | axes if axes else data
-    if name:
-        return { name: value}
-    return value
+def to_json(ndarray, axes=None):
+    data = ndarray.tolist()
+    dtype = ndarray.dtype.name
+    if axes: 
+        axes_name = list(axes)
+        axes_values = list(axes.values())
+        return {':ndarray': [data, dtype], 'xnames': axes_name, 'xvalues': axes_values}
+    return [data, dtype]
         
+def from_json(js):
+    if isinstance(js, dict):
+        return (np.array(js[':ndarray'][0], dtype=js[':ndarray'][1]), 
+                {name: val for name, val in zip(js['xnames'], js['xvalues'])})
+    return (np.array(js[0], dtype=js[1]), {})
 
-def to_json_tab(ndarray, axes=None, name=None):
+def to_json_tab(ndarray, axes=None):
     period = ndarray.shape
     dim = ndarray.ndim
 
-    coefi = ndarray.size // period[0]
-    coef = [coefi]
-    for per in period[1:]:
+    coefi = ndarray.size
+    coef = []
+    for per in period:
         coefi = coefi // per
         coef.append(coefi)
         
     axes_name = list(axes) if axes else ['axe' + str(i) for i in range(dim)]
-    axes_value = list(axes.values()) if axes else [list(range(period[i])) for i in range(dim)]
+    axes_value = list(axes.values()) if isinstance(axes, dict) else [list(range(period[i])) for i in range(dim)]
     axes = {name: [val, [coe]] for name, val, coe in zip(axes_name, axes_value, coef)}
-    name = 'val' if not name else name
-    return axes | {name: ndarray.flatten().tolist()}    
+    return axes | {'::' + ndarray.dtype.name: ndarray.flatten().tolist()}    
 
 def from_json_tab(js):
     ntv = Ntv.obj(js)
     if len(ntv) == 0:
         return
-    decode = [list(NtvUtil.decode_ntv_tab(ntvf, Sfield.ntv_to_val)) for ntvf in ntv]
-    leng = max(field[6] for field in decode)
-    format_fields = NtvConnector.format_field(decode, leng)    
+    decode = [list(NtvUtil.decode_ntv_tab(ntvf, Sfield.l_to_i)) for ntvf in ntv]
+    print(decode)
+    print(type(decode[2][0]))
+    #leng = max(field[6] for field in decode)
+    #format_fields = NtvConnector.format_field(decode, leng)    
     
     shape = []
-    index = None
-    for ind, (forma, field) in enumerate(zip(format_fields, decode)):
-        if forma == 'primary':
+    axes_name = []
+    axes_values = []
+    coef = []
+    ndarray = None
+    for field in decode:
+        if field[5]:
             shape.append(len(field[2]))
+            coef.append(field[5])
+            axes_values.append(field[2])
+            axes_name.append(field[0])
         else:
-            index = ind
-    return np.array(decode[index][2]).reshape(shape)
-
-
-'''a = np.arange(1,25).reshape((2,3,2,2))
-js = to_json_tab(a, axes)
-print(js)
-nt = Sdataset.ntv(js)
-
-print(nt.to_ntv())
-print(npd.read_json(js))
-print(from_json_tab(js))'''
+            ndarray = np.array([ntv.val for ntv in field[2]], dtype=field[1])
+    coef, shape, axes_name, axes_values = list(zip(*sorted(zip(coef, shape, axes_name, axes_values), reverse=True)))
+    return (ndarray.reshape(shape), 
+            {name: val for name, val in zip(axes_name, axes_values)})
