2c9c2c99010de0c81dd7e15c849276497ac37f29
Philippe THOMY <philippe@loco-labs.io>
2024-03-03 15:35:33 +0100

    Update data_array.py

--- python/Tests/data_array.py
+++ python/Tests/data_array.py
@@ -6,7 +6,9 @@ Created on Sun Mar  3 11:14:40 2024
 """
 
 import numpy as np
+import pandas as pd
 from abc import ABC, abstractmethod
+from copy import copy
 
 class Darray(ABC):
     ''' Representation of a one dimensional Array'''
@@ -53,19 +55,66 @@ class Darray(ABC):
         ''' Copy all the data '''
         return self.__class__(self)
     
+    @staticmethod 
+    @abstractmethod
+    def read_json(self):
+        pass
+    
     @abstractmethod
     def to_json(self):
         pass
     
+    @property
     @abstractmethod
     def values(self):
         pass
     
+    @property
     @abstractmethod
     def len_val(self):
         pass
     
-def Dfull(Darray):    
+class Dfull(Darray):    
     ''' Representation of a one dimensional Array with full representation'''
     def __init__(self, data):
         super().__init__(data)
+    
+    def to_json(self):
+        return self.data.astype(str).tolist()
+    
+    @property 
+    def values(self):
+        return self.data
+    
+    @property
+    def len_val(self):
+        return len(self.values)
+    
+    @staticmethod 
+    def read_json(val):
+        return Dfull(val)
+    
+class Dcomplete(Darray):    
+    ''' Representation of a one dimensional Array with full representation'''
+    def __init__(self, data, coding=None):
+        if not coding:
+            data = pd.Series(data).astype('category')
+            coding = np.array(data.cat.codes)
+            data = np.array(data.cat.categories)
+        super().__init__(data, None, coding)
+    
+    def to_json(self):
+        return [self.data.astype(str).tolist(), self.coding.tolist()]
+    
+    @property 
+    def values(self):
+        categ = pd.CategoricalDtype(categories=pd.Series(self.data))
+        return np.array(pd.Categorical.from_codes(codes=self.coding, dtype=categ))
+    
+    @property
+    def len_val(self):
+        return len(self.values)
+    
+    @staticmethod 
+    def read_json(val):
+        return Dfull(val)
